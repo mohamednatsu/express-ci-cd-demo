f0b721c1e704ce4b283ab75a5364243508ccba5d
mohamednatsu <msalhnwr@gmail.com>
2025-07-20 18:27:31 +0300

    add pipline ci cd actions

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,18 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /', () => {
+       let server;
+
+       beforeAll((done) => {
+              server = app.listen(0, () => { // Use 0 to get a random available port
+                     done();
+              });
+       });
+
+       afterAll((done) => {
+              server.close(done);
+       });
+
        it('should return welcome message', async () => {
               const res = await request(app).get('/');
               expect(res.statusCode).toEqual(200);
