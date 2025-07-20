3987e41f3fe41fe83ec3c558719d66dd12dfe798
mohamednatsu <msalhnwr@gmail.com>
2025-07-20 18:21:55 +0300

    add pipline ci cd actions

---
+++ src/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
